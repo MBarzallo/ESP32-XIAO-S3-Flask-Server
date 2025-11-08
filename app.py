cf9f0104571b4fdc16952afb20886ee536c06384
Vladimir Espartaco Robles Bykbaev <sparthakus@gmail.com>
2025-11-08 23:03:12 +0000

    The app.py have updated to include the convolution between a 3x3 median filter and the noise image. The image 'Screenshot_20251108_171053.png' is added.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # Version: 1.0
 # Date: 2024-10-20
 # Description: A simple example to process video captured by the ESP32-XIAO-S3 or ESP32-CAM-MB in Flask.
-
+# Updated: 2025-08-11
 
 from flask import Flask, render_template, Response, stream_with_context, Request
 from io import BytesIO
@@ -11,10 +11,13 @@ from io import BytesIO
 import cv2
 import numpy as np
 import requests
+import torch
+import torch.nn.functional as F
 
 app = Flask(__name__)
+
 # IP Address
-_URL = 'http://10.0.0.3'
+_URL = 'http://10.0.0.11'
 # Default Streaming Port
 _PORT = '81'
 # Default streaming route
@@ -23,9 +26,16 @@ SEP = ':'
 
 stream_url = ''.join([_URL,SEP,_PORT,_ST])
 
-
+# Method to capture the video generated from the ESP32-XIAO-S3 mini cam
 def video_capture():
     res = requests.get(stream_url, stream=True)
+
+    median_mask = torch.ones(3, 3, dtype = torch.float32)/9.0
+
+    kernel = median_mask.unsqueeze(0).unsqueeze(0)
+    convolved_output = None
+    img_output = None
+
     for chunk in res.iter_content(chunk_size=100000):
 
         if len(chunk) > 100:
@@ -35,6 +45,7 @@ def video_capture():
                 gray = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
                 N = 537
                 height, width = gray.shape
+
                 noise = np.full((height, width), 0, dtype=np.uint8)
                 random_positions = (np.random.randint(0, height, N), np.random.randint(0, width, N))
                 
@@ -42,9 +53,22 @@ def video_capture():
 
                 noise_image = cv2.bitwise_or(gray, noise)
 
-                total_image = np.zeros((height, width * 2), dtype=np.uint8)
+                img_tensor = torch.from_numpy(noise_image.astype(np.float32)).unsqueeze(0).unsqueeze(0)
+
+                convolved_output = F.conv2d(
+                    input = img_tensor,
+                    weight = kernel,
+                    padding = 1,
+                    bias = None
+                )
+
+                img_output = convolved_output.cpu().numpy().squeeze()
+                img_output = cv2.convertScaleAbs(img_output)
+
+                total_image = np.zeros((height, width * 3), dtype=np.uint8)
                 total_image[:, :width] = gray
-                total_image[:, width:] = noise_image
+                total_image[:, width:width*2] = noise_image
+                total_image[:, width*2:] = img_output
 
                 (flag, encodedImage) = cv2.imencode(".jpg", total_image)
                 if not flag:
